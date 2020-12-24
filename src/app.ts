0a2bf762b9a149fdc443c8bdc9d5b9cc9104d860
thomasYZT <624524240@qq.com>
2020-12-24 14:31:56 +0800

    项目初始化

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import store from './store'
+import TaroUiVue from 'taro-ui-vue/src'
+import 'taro-ui-vue/dist/style/index.scss'
+import './app.scss'
+Vue.use(TaroUiVue)
+
+// Vue.config.productionTip = false
+
+const App = new Vue({
+  store,
+  render(h) {
+    // this.$slots.default 是将要会渲染的页面
+    return h('block', this.$slots.default)
+  }
+})
+
+export default App
